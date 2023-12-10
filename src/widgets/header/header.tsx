9fdce7a2830d92c4f5df14837458bebfb7be1968
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2023-12-10 23:05:25 +0600

    Setting up a project

---
+++ src/widgets/header/header.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-gray-600 h-12 '>
+        <div className="flex justify-between max-w-5xl m-auto py-3 ">
+
+            <div className="">
+                <h1 className='items-center'>Saed</h1>
+            </div>
+
+            <div className="flex justify-between">
+                <div className="px-5">
+                    <h1>()</h1>
+                </div>
+                <div className="px-5">
+                    <h1>[]</h1>
+                </div>
+            </div>
+
+            <div className="">
+                Login
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
